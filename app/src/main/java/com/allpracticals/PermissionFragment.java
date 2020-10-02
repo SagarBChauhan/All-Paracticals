cdd2746c5ab75b9c4d132d59da1bc55994a61007
Sagar Chauhan <chauhansagar373@gmail.com>
2020-10-02 17:07:14 +0530

    Runtime permission applied

--- app/src/main/java/com/allpracticals/PermissionFragment.java
+++ app/src/main/java/com/allpracticals/PermissionFragment.java
@@ -15,25 +15,58 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.Objects;
+
 public class PermissionFragment extends Fragment {
     public static final String TAG = "PermissionFragment";
     private static final int REQUEST_LOCATION = 0;
     private static final int REQUEST_STORAGE = 1;
     private static final int REQUEST_CAMERA = 2;
     private static final int REQUEST_ALL = 3;
+    private static String[] permissions;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        init();
+        checkAllPermission();
+    }
+
+    private void init() {
+        permissions = new String[]{
+                Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION};
+    }
+
+    private void checkAllPermission() {
+        if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "checkSelfPermission: ALL");
             requestAllPermissions();
+        } else {
+            Log.i(TAG, "else checkSelfPermission: ALL");
         }
+    }
+
+    private void requestAllPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()), Manifest.permission.CAMERA)
+                || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            Log.i(TAG, "shouldShowRequestPermissionRationale: ALL");
+            Snackbar.make(requireActivity().findViewById(R.id.permission_fragment), "Required some permission to run application properly.", Snackbar.LENGTH_INDEFINITE)
+                    .setAction(getString(R.string.positive_button), v -> ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ALL)).show();
+        } else {
+            Log.i(TAG, "else shouldShowRequestPermissionRationale: ALL");
+            ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()), permissions, REQUEST_ALL);
+        }
+    }
+
+    private void checkLocationPermission() {
         Log.i(TAG, "->: ACCESS_FINE_LOCATION");
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "checkSelfPermission: ACCESS_FINE_LOCATION");
 //            AlertDialog.Builder builderLocation = new AlertDialog.Builder(getContext())
 //                    .setTitle("Required Location permission")
@@ -62,8 +95,11 @@ public class PermissionFragment extends Fragment {
         } else {
             Log.i(TAG, "else checkSelfPermission: ACCESS_FINE_LOCATION");
         }
+    }
+
+    private void checkStoragePermission() {
         Log.i(TAG, "->: READ_EXTERNAL_STORAGE/WRITE_EXTERNAL_STORAGE");
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+        if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
                 || ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "checkSelfPermission: READ_EXTERNAL_STORAGE/WRITE_EXTERNAL_STORAGE");
 //            AlertDialog.Builder builderStorage = new AlertDialog.Builder(getContext())
@@ -93,8 +129,11 @@ public class PermissionFragment extends Fragment {
         } else {
             Log.i(TAG, "else checkSelfPermission: READ_EXTERNAL_STORAGE/WRITE_EXTERNAL_STORAGE");
         }
+    }
+
+    private void checkCameraPermission() {
         Log.i(TAG, "->: CAMERA");
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "checkSelfPermission: CAMERA");
 //            AlertDialog.Builder builderCamera = new AlertDialog.Builder(getContext())
 //                    .setTitle("Required Camera permission")
@@ -125,22 +164,11 @@ public class PermissionFragment extends Fragment {
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_permission, container, false);
-    }
-
     private void requestLocationPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()), Manifest.permission.ACCESS_FINE_LOCATION)) {
             Log.i(TAG, "shouldShowRequestPermissionRationale: ACCESS_FINE_LOCATION");
             Snackbar.make(requireActivity().findViewById(R.id.permission_fragment), "Location permission", Snackbar.LENGTH_INDEFINITE)
-                    .setAction(getString(R.string.positive_button), new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-                        }
-                    })
+                    .setAction(getString(R.string.positive_button), v -> ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION))
                     .show();
         } else {
             Log.i(TAG, "else shouldShowRequestPermissionRationale: ACCESS_FINE_LOCATION");
@@ -149,53 +177,30 @@ public class PermissionFragment extends Fragment {
     }
 
     private void requestStoragePermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()), Manifest.permission.READ_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             Log.i(TAG, "shouldShowRequestPermissionRationale: READ_EXTERNAL_STORAGE/WRITE_EXTERNAL_STORAGE");
             Snackbar.make(requireActivity().findViewById(R.id.permission_fragment), "Storage permission required", Snackbar.LENGTH_INDEFINITE)
-                    .setAction(getString(R.string.positive_button), new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE);
-                        }
-                    }).show();
+                    .setAction(getString(R.string.positive_button), v -> ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE)).show();
         } else {
             Log.i(TAG, "else shouldShowRequestPermissionRationale: READ_EXTERNAL_STORAGE/WRITE_EXTERNAL_STORAGE");
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE);
+            ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_STORAGE);
         }
     }
 
     private void requestCameraPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CAMERA)) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()), Manifest.permission.CAMERA)) {
             Log.i(TAG, "shouldShowRequestPermissionRationale: CAMERA");
             Snackbar.make(requireActivity().findViewById(R.id.permission_fragment), "CAMERA permission required", Snackbar.LENGTH_INDEFINITE)
-                    .setAction(getString(R.string.positive_button), new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
-                        }
-                    }).show();
+                    .setAction(getString(R.string.positive_button), v -> ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA)).show();
         } else {
             Log.i(TAG, "else shouldShowRequestPermissionRationale: CAMERA");
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
         }
     }
 
-    private void requestAllPermissions() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.CAMERA)
-                || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                || ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
-            Log.i(TAG, "shouldShowRequestPermissionRationale: ALL");
-            Snackbar.make(requireActivity().findViewById(R.id.permission_fragment), "CAMERA, STORAGE, LOCATION permission required", Snackbar.LENGTH_INDEFINITE)
-                    .setAction(getString(R.string.positive_button), new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ALL);
-                        }
-                    }).show();
-        } else {
-            Log.i(TAG, "else shouldShowRequestPermissionRationale: ALL");
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ALL);
-        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_permission, container, false);
     }
-
 }
