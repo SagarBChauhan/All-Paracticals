3dbd034f05c5afc5f2cb41b1beaca7b683263007
SagarBChauhan <Chuahansagar373@gmail.com>
2020-10-12 16:27:32 +0530

    API calling demo using Retrofit

---
+++ app/src/main/java/com/allpracticals/APIInterface.java
@@ -0,0 +1,28 @@
+package com.allpracticals;
+
+import com.allpracticals.mojo.MultipleResource;
+import com.allpracticals.mojo.User;
+import com.allpracticals.mojo.UserList;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface APIInterface {
+    @GET("/api/unknown")
+    Call<MultipleResource> doLGetListResources();
+
+    @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @GET("/api/users?")
+    Call<UserList> doGetUserList(@Query("page") String page);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);
+}
---
+++ app/src/main/java/com/allpracticals/mojo/CreateUserResponse.java
@@ -0,0 +1,15 @@
+package com.allpracticals.mojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CreateUserResponse {
+    @SerializedName("name")
+    public String name;
+    @SerializedName("job")
+    public String job;
+    @SerializedName("id")
+    public String id;
+    @SerializedName("createdAt")
+    public String createdAt;
+
+}
