a4a04b334ede0cf12020a9296283816c57e60ef6
SagarBChauhan <Chuahansagar373@gmail.com>
2020-10-12 18:20:33 +0530

    Revert "Revert "API calling demo using Retrofit and SSP, SDP implemented
    This reverts commit 8e1d24e5

---
+++ app/src/main/java/com/allpracticals/mojo/User.java
@@ -0,0 +1,19 @@
+package com.allpracticals.mojo;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("name")
+    public String name;
+    @SerializedName("job")
+    public String job;
+    @SerializedName("id")
+    public String id;
+    @SerializedName("createdAt")
+    public String createdAt;
+
+    public User(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+}
